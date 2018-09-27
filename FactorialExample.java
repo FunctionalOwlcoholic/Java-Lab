7cfaadd344f9caf9de0d7f8255874fca869bbbc6
FunctionalOwlcoholic <43358255+FunctionalOwlcoholic@users.noreply.github.com>
2018-09-27 18:09:34 -0400

    Add files via upload

---
+++ FactorialExample.java
@@ -0,0 +1,40 @@
+
+public class FactorialExample
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Recursive:");
+      for (int i = 0; i < 13; i++)
+      {
+         System.out.println(i + "! = " + recursiveFactorial(i));
+      }
+      System.out.println("\nIterative:");
+      for (int i = 0; i < 13; i++)
+      {
+         System.out.println(i + "! = " + iterativeFactorial(i));
+      }
+   }
+   
+   public static long recursiveFactorial(int n)
+   {
+      if (n < 1)
+      {
+         return 1;
+      }
+      return n * recursiveFactorial(n - 1);
+   }
+   
+   public static long iterativeFactorial(int n)
+   {
+      if (n < 1)
+      {
+         return 1;
+      }
+      long f = n;
+      while (n > 1)
+      {
+         f *= --n;
+      }
+      return f;
+   }
+}
