573951e21cc1f6a0949466c8d9ac94ff35a1572d
FunctionalOwlcoholic <43358255+FunctionalOwlcoholic@users.noreply.github.com>
2018-09-28 17:44:23 -0400

    Add files via upload

---
+++ BindingExample.java
@@ -0,0 +1,40 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+
+public class BindingExample extends Application
+{
+   @Override
+   public void start(Stage mainStage)
+   {
+      // root pane
+      GridPane rootPane = new GridPane();
+      rootPane.setMinSize(150, 150);
+      
+      // buttons
+      for (int i = 0; i < 3; i++)
+      {
+         for (int j = 0; j < 3; j++)
+         {
+            Button button = new Button();
+            button.setMinSize(50, 50);
+            
+            // bindings
+            button.prefWidthProperty().bind(rootPane.widthProperty());
+            button.prefHeightProperty().bind(rootPane.heightProperty());
+            
+            rootPane.add(button, j, i);
+         }
+      }
+      
+      // main scene
+      Scene mainScene = new Scene(rootPane, 640, 480);
+      
+      // main stage
+      mainStage.setScene(mainScene);
+      mainStage.setTitle("Hello Binding");
+      mainStage.show();
+   }
+}
